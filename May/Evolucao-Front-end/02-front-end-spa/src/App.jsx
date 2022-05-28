4fd3dcd00920598266368fdbde0d37979ed01831
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-05-28 14:32:26 -0400

    Save

---
+++ May/Evolucao-Front-end/02-front-end-spa/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <ul>
+    <li> Joao Victor</li>
+    <li> Gabriel </li>
+    <li> Marcos</li>
+  </ul>
+    )
+}
+
+export default App
