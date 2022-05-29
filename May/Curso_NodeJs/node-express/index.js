7719de25fdbcd48a47a0855f9ec6ff8103ceeff5
João Victor S. De Moraes <joao337.jvs@gmail.com>
2022-05-29 15:47:00 -0400

    Ajustes de pastas

--- May/Curso_NodeJs/node-express/index.js
+++
@@ -1,9 +0,0 @@
-const express = require ('express')
-
-const userRoute = require('./routes/userRoute')
-
-const app = express()
-const port = 3000
-app.get('/',(req,res)=>res.send('Olá World pelo Express'))
-
-app.listen(port, ()=> console.log('Api rodando na porta 3000')) 
